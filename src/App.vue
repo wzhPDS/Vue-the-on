4ca728b29730a25217f7ef816fc76fe1c99ac9e0
胖弟叔 <1007949251@qq.com>
2019-06-21 15:00:45 +0800

    后台管理系统

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- 这个router-view就是以后其它组件所能放置的位置 -->
+    <router-view/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style lang="less">
+// #app {
+//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
+//   -webkit-font-smoothing: antialiased;
+//   -moz-osx-font-smoothing: grayscale;
+//   text-align: center;
+//   color: #2c3e50;
+//   margin-top: 60px;
+// }
+</style>
